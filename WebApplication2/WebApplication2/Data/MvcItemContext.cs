9397de66a40b5c0d297bcb1243e8fb85e66835b7
Jesslyn Khamsoi (Tata Consultancy Services Ltd) <v-jekha@microsoft.com>
2020-05-25 16:21:51 -0700

    New project with authentication - merged and deleted old version

---
+++ WebApplication2/WebApplication2/Data/MvcItemContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class MvcItemContext: DbContext
+    {
+        public MvcItemContext(DbContextOptions<MvcItemContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<Item> Item { get; set; }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //[Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+    }
+}
